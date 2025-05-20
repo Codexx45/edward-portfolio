8dffc2252b8ea6f8524e9304f09ca4b553f3ad96
Codexx45 <edwardshildon081@gmail.com>
2025-05-20 14:22:30 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Myportfolio/',
   plugins: [
     tailwindcss(),
     react()
   ],
+  base: '/edward-portfolio',
 })
